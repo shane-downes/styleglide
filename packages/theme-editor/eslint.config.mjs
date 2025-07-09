efea7593d1518c6f5045164afa2ac6196d3a106a
shanedownes <shanedownes01@gmail.com>
2025-07-09 17:54:26 +0100

    fix(workflows): theme-editor lint

--- packages/theme-editor/eslint.config.mjs
+++ packages/theme-editor/eslint.config.mjs
@@ -1,4 +1,9 @@
 import { config } from "@workspace/eslint-config/base";
 
 /** @type {import("eslint").Linter.Config} */
-export default config;
+export default [
+  ...config,
+  {
+    ignores: ["dist/**", "tsup.config.bundled_*"],
+  },
+];
